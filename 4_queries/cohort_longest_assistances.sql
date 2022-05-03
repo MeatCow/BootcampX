58ac169433b99d7eb580b54346567406be212715
Matthieu Pauze <matt.pauze@gmail.com>
2022-05-03 17:59:17 -0400

    Feature: Implement queries for w5-d2

---
+++ 4_queries/cohort_longest_assistances.sql
@@ -0,0 +1,13 @@
+SELECT *
+FROM (
+    SELECT cohorts.name,
+      AVG(
+        assistance_requests.completed_at - assistance_requests.started_at
+      ) AS average_assistance_time
+    FROM assistance_requests
+      JOIN students ON students.id = student_id
+      JOIN cohorts ON cohorts.id = students.cohort_id
+    GROUP BY cohorts.name
+    ORDER BY average_assistance_time desc
+    LIMIT 1
+  ) AS average_assistance_times;
